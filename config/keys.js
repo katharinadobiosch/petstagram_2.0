4ab8a82cf40826247524fa81ebed3561cd4a04f0
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2024-05-15 15:11:59 +0200

    wip

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,10 @@
-if(process.env.NODE_ENV==='production'){
-    module.exports = require('./prod')
-}else{
-    module.exports = require('./dev')
+if (process.env.NODE_ENV === "production") {
+    module.exports = require("./prod");
+} else {
+    module.exports = require("./dev");
 }
+
+// module.exports = {
+//     MONGOURI:
+//         "mongodb+srv://kathi:tUsyNB0GAEFl7BqQ@petstagram.8enphzf.mongodb.net/?retryWrites=true&w=majority&appName=petstagram",
+// };
